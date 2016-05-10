664027d26978c45b170078413ecda53d73701481
Alex Forbes <alforbes@gumtree.com>
2016-05-10 16:39:14 +0100

    Bump build to jessie

--- debuild.sh
+++ debuild.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 find ./ -iname '*.pyc' -exec rm {} \;
-git buildpackage --git-dist=wheezy --git-debian-branch=master --git-ignore-new --git-sign-tags
+git buildpackage --git-dist=jessie --git-debian-branch=master --git-ignore-new --git-sign-tags
